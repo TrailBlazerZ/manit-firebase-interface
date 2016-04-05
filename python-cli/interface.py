50a7d0d9621a393ee562fd03e30e5143265216fb
Barun <emailbarung@gmail.com>
2016-04-06 02:50:36 +0530

    Get the action id and trigger the relevant json

--- python-cli/interface.py
+++ python-cli/interface.py
@@ -11,6 +11,14 @@ valn = f.readline()
 vale = f.readline()
 idn, ide = int(valn), int(vale)
 f.seek(0,0)
+listStories = []
+listEvents = []
+
+if firebase.get('/TopStories','v0') is not None :
+    listStories = firebase.get('/TopStories','v0')
+if firebase.get('/TopEvents','v0') is not None:
+    listEvents = firebase.get('/TopEvents','v0')
+
 
 if (raw_input("\n 1. News \t 2. Events \n Enter among the above options:") == '2'):
 	flag = 1
@@ -20,28 +28,34 @@ if (raw_input("\n 1. News \t 2. Events \n Enter among the above options:") == '2
 	ide +=  1
 	idn -=  1
 	eventInx = str(ide)
+	listEvents.insert(0,ide)
 
-idn += + 1
+idn += 1
+listStories.insert(0,idn)
+
 newSubmittedBy= raw_input("AUTHOR: ")
 newType = raw_input("TYPE: ")
 newTitle = raw_input("TITLE: ")
 newPickUpLine = raw_input("STORY LINE: ")
 newContent = raw_input("CONTENT: ")
 newUrl = raw_input("URL: ")
-
+print listStories
 
 if flag == 1 :
-	data={'id':eventInx, 'title':newTitle, 'content':newContent, 'pickupline':newPickUpLine, 'by':newSubmittedBy, 'type':newType, 'url':newUrl, 'date':newEventDate, 'time':newEventTime, 'location':newEventVenue}
-	result = firebase.put('/event', eventInx, data)
+    print listStories
+    resulte = firebase.put('/TopEvents', "v0", listEvents) 
+    data = {'id':eventInx, 'title':newTitle, 'story':newContent, 'pickupline':newPickUpLine, 'by':newSubmittedBy, 'type':newType, 'url':newUrl, 'date':newEventDate, 'time':newEventTime, 'location':newEventVenue}
+    result = firebase.put('/event', eventInx, data)
+    print (resulte)
 else:
-	newsIndex = str(idn)
-	data={'id':newsIndex, 'title':newTitle, 'content':newContent, 'pickupline':newPickUpLine, 'by':newSubmittedBy, 'type':newType, 'url':newUrl}
-	result = firebase.put('/news', newsIndex, data)
+    newsIndex = str(idn)
+    print (listEvents)
+    resultn = firebase.put('/TopStories', "v0", listStories)
+    data={'id':newsIndex, 'title':newTitle, 'story':newContent, 'pickupline':newPickUpLine, 'by':newSubmittedBy, 'type':newType, 'url':newUrl}
+    result = firebase.put('/news', newsIndex, data)
+    print (resultn)
 
 f.write(str(idn) + '\n' + str(ide))
 f.close()
 
 print (result)
-
-
-
